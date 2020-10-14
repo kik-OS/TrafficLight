7829bab8fd2171714cee01dc7722f40f834adf39
Nikita Gvozdikov <pounddnuop@mail.ru>
2020-10-14 13:17:44 +0300

    final

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var redColorView: UIView!
-    
     @IBOutlet var orangeColorView: UIView!
-    
     @IBOutlet var greenColorView: UIView!
     @IBOutlet var toggleColorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         redColorView.layer.cornerRadius = 50
@@ -25,6 +24,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func toggleColorPressedButton() {
+        toggleColorButton.setTitle("Next", for: .normal)
+        
+        if redColorView.alpha < 1 &&
+            orangeColorView.alpha < 1 &&
+            greenColorView.alpha < 1 {
+            redColorView.alpha = 1
+        }
+        else if redColorView.alpha == 1 {
+            redColorView.alpha = 0.3
+            orangeColorView.alpha = 1
+        }
+        else if orangeColorView.alpha == 1 {
+            orangeColorView.alpha = 0.3
+            greenColorView.alpha = 1
+        }else {
+            redColorView.alpha = 1
+            greenColorView.alpha = 0.3
+        }
     }
 }
 
